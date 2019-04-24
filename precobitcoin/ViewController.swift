312a891cf198fb13619e15d8fb2cf22d507d7036
Moises <moises@moisess-mac.local>
2019-04-23 18:58:45 -0700

    Initial Commit

---
+++ precobitcoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  precobitcoin
+//
+//  Created by Moises on 23/04/19.
+//  Copyright © 2019 Moises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
