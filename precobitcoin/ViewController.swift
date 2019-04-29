d85bbbfa0842f0b22112f2c7f1c8cb09d186fdad
Moises Olimpio <molimpio@hotmail.com.br>
2019-04-29 01:55:44 -0700

    buscado valor compra bitcoin em moeda br

--- precobitcoin/ViewController.swift
+++ precobitcoin/ViewController.swift
@@ -14,10 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         if let url = URL(string: "https://blockchain.info/pt/ticker") {
-            let tarefa = URLSession.shared.dataTask(with: url) { (dados, response, error) in
+            let tarefa = URLSession.shared.dataTask(with: url) { (dados, resquest, error) in
                 
                 if error == nil {
-                    print("sucesso na requisicao")
+                    
+                    if let dadosResponse = dados {
+                        
+                        do {
+                            if let objJSON = try JSONSerialization.jsonObject(with: dadosResponse, options: []) as? [String: Any] {
+                                if let brl = objJSON["BRL"] as? [String: Any]  {
+                                    if let precoCompra = brl["buy"] as? Double {
+                                        print(precoCompra)
+                                    }
+                                }
+                            }
+                            
+                        } catch {
+                            print("Erro ao formatar retorno")
+                        }
+                    }
                 } else {
                     print("erro na requisicao")
                 }
